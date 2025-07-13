ba153de61f1c07314c27fef9e38a0dbf01730d5a
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-07-13 11:55:37 -0400

    08 removeFromArray solution
    using the filter method to remove any specified amount of elements from
    an array and then return the array without those elements

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+// TODO: removes a single specified element from the array
+// NOTE: creating a function that takes an array and
+const removeFromArray = function (arr, ...rmNums) {
+  const filteredArr = arr.filter((rmNum) => !rmNums.includes(rmNum));
+  return filteredArr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
