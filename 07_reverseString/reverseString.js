50a3f7fc0012058e579df67c7fd7870b1157162c
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-07-12 21:53:39 -0400

    Reverse String solution
    using  spread '...' reverse(), and join method to reverse strings

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+// NOTE: creating a function that takes an array as an arguement
+const reverseString = function (str) {
+  // NOTE: spreading our string into an array so we can reverse the order of the letters
+  const reversedString = [...str].reverse().join("");
+  return reversedString;
 };
 
+console.log(reverseString("hello there"));
 // Do not edit below this line
 module.exports = reverseString;
