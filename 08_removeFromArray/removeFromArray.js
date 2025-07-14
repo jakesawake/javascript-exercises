a5e10cc44cc6f967dc499ccda246cf386b6abcf0
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-07-14 18:26:47 -0400

    adding notes to 08_removeFromArray solution
    adding comments explaining 08_removeFromArray solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,7 +1,8 @@
 // TODO: removes a single specified element from the array
-// NOTE: creating a function that takes an array and
+// NOTE: creating a function that takes an array and an indefinite number of arguments as a single array
+// in short anything passed in as an arguement to rmNums will be passed into a single array
 const removeFromArray = function (arr, ...rmNums) {
-  const filteredArr = arr.filter((rmNum) => !rmNums.includes(rmNum));
+  const filteredArr = arr.filter((rmNum) => !rmNums.includes(rmNum)); // NOTE: using the filter method to say if the current rmNum DOES NOT include the rmNum specified in the arguement, since this returns true, it is passed into the filteredArr
   return filteredArr;
 };
 
