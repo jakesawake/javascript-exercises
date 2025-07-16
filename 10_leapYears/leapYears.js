f2ad71561ef24b0299666b659c26e4d761b45149
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-07-16 18:59:57 -0400

    11_tempConversion solution
    using different Javascript methods, constructors and conversion formulas
    to create temp conversion functions

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,5 @@
 const leapYears = function (year) {
+  // NOTE: if statement that handles all edge cases of leap year
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
   } else {
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // NOTE: formula for converting fahrenheit to celsius
+  let convertedTempCelsius = (5 / 9) * (temp - 32);
+  // NOTE: using Math.round() to return a number, also rounding halfway values up
+  Math.round((convertedTempCelsius * 10) / 10);
+  // NOTE: using toFixed() to return 1 decimal place,
+  // since toFixed() returns a string, we wrap our result
+  // in a Number() constructor so that we convert our string
+  // value to a primitive type
+  return Number(convertedTempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+// NOTE: same thing as above but different formula to
+// convert celsius to fahrenheit
+const convertToFahrenheit = function (temp) {
+  let convertedTempFahrenheit = (9 / 5) * temp + 32;
+  Math.round((convertedTempFahrenheit * 10) / 10);
+  return Number(convertedTempFahrenheit.toFixed(1));
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
