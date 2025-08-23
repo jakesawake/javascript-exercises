476ee7cd21d5c6a5dcedd0eb72fd9a905a10f79d
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-08-23 14:29:57 -0400

    Calculator solution
    Solution for the calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,50 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+console.log(add(1, 2));
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+console.log(subtract(5, 3));
+
+const sum = function (arr) {
+  return arr.reduce((acc, curr) => {
+    acc += curr;
+    return acc;
+  }, 0);
 };
 
-const multiply = function() {
+console.log(sum([1, 2, 3]));
 
+const multiply = function (num) {
+  return num.reduce((acc, curr) => (acc *= curr));
 };
 
-const power = function() {
-	
+console.log(multiply([2, 4]));
+
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+console.log(power(5, 5));
+
+const factorial = function (num) {
+  if (typeof num != "number") {
+    console.log("enter a number only");
+  } else if (num <= 0) {
+    console.log("enter a number greater than 0");
+  }
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
+
+console.log(factorial(3));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +53,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
