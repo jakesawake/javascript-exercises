d6dab4f2a4930222324bdc703ee085caffcac0d5
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-08-27 19:43:11 -0400

    Fibonacci solution
    using Iterative approach with two values (prev & curr) updating them
    each step to get the next fibonacci number

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  // handling edge cases below
+  if (num === 0 || num === "0") {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  } else if (typeof num === "string") {
+    num = Number(num);
+  }
+  // prev and curr representing two consecutive fibonacci numbers at any moment
+  let prev = 0; // holds F(0)
+  let curr = 1; // holds F(1)
+  // loop starts at 2 because prev and curr are already known
+  for (let i = 2; i <= num; i++) {
+    let result = prev + curr; // computing the next fibonacci number
+    prev = curr; // move prev forward so it becomes what curr was
+    curr = result; // move curr forward so it becomes the newly computed number
+    // after the loop, curr is the nth Fibonacci number (F(num))
+  }
+  return curr;
 };
 
+console.log(fibonacci(-25));
 // Do not edit below this line
 module.exports = fibonacci;
