8dffdf75dca6e7b372c73c3de6972d069a5e3bd0
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-08-30 10:35:51 -0400

    Solution for 15_getTheTitles
    Using map to get a new array from array of objects

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+// passing an array of objects as an arguement
+const getTheTitles = function (arrOfObj) {
+  // using map to return a new arr, returning callback
+  return arrOfObj.map((obj) => {
+    let newArr;
+    newArr = obj.title; // taking the title of each obj and putting it in newArr
+    return newArr; // returning inner function
+  });
 };
 
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
